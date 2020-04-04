7efe6a86be08fe88a508786ba55d35ddb3bc247e
Baishali Ghosh <baishali.ghosh@foyr.com>
2020-04-05 00:55:47 +0530

    MinimumSwaps

---
+++ Arrays/MinimumSwaps2.cs
@@ -0,0 +1,45 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+        int swapsNeeded = 0;
+        for(int i=0; i<arr.Length; i++){
+            while((arr[i] - 1) != i ){
+                int temp = arr[i];
+                arr[i] = arr[arr[i] - 1];
+                arr[temp - 1] = temp;
+                swapsNeeded++;
+            }
+        }    
+        return swapsNeeded;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int res = minimumSwaps(arr);
+
+        textWriter.WriteLine(res);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
